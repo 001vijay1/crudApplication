3746a337afa3b5d3127f8c65a782fe0700dfd39f
001vijay1 <52908898+001vijay1@users.noreply.github.com>
2020-01-12 19:56:12 +0530

    Update admin.py

--- curd/crudApplication/admin.py
+++ curd/crudApplication/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from .models import Book,BookProfile,Contact
+from django.contrib.admin.options import ModelAdmin
 
 # Register your models here.
-admin.site.register(Book)
+class BookAdmin(ModelAdmin):
+    list_display = ['book_name','publish_date']
+    search_fields = ['book_name']
+
+admin.site.register(Book,BookAdmin)
 admin.site.register(BookProfile)
 admin.site.register(Contact)
+
+
